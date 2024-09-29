214121dc958a65a3767be2980d598b1414117fc7
yarednicolas23 <yarednicolas@gmail.com>
2024-09-29 18:54:21 -0500

    first commit

---
+++ MeliChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeliChallenge
+//
+//  Created by Yared Nicolas Toro C on 29/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeliChallenge/MeliChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliChallengeApp.swift
+//  MeliChallenge
+//
+//  Created by Yared Nicolas Toro C on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MeliChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
